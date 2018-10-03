eb1a4d4ebac5aefceb9675d67225396deba5f3e7
Ziyang <frank16874@gmail.com>
2018-10-03 16:30:55 +0800

    Update to 4.8.2

--- vsplatego.sh
+++ vsplatego.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -O /var/www/phpMyAdmin-4.8.1-english.tar.gz http://test-1251457471.cosgz.myqcloud.com/phpMyAdmin-4.8.1-english.tar.gz && rm -rf /var/www/html/* &&  tar -zxf /var/www/phpMyAdmin-4.8.1-english.tar.gz -C /var/www/html && mv /var/www/html/phpMyAdmin-4.8.1-english/* /var/www/html/ && rm -rf /var/www/html/phpMyAdmin-4.8.1-english;
+wget -O /var/www/phpMyAdmin-4.8.2-english.tar.gz https://files.phpmyadmin.net/phpMyAdmin/4.8.2/phpMyAdmin-4.8.2-english.tar.gz && rm -rf /var/www/html/* &&  tar -zxf /var/www/phpMyAdmin-4.8.2-english.tar.gz -C /var/www/html && mv /var/www/html/phpMyAdmin-4.8.2-english/* /var/www/html/ && rm -rf /var/www/html/phpMyAdmin-4.8.2-english;
 chmod -R 777 /var/www/html
